86c6d8caf36d503bf9d90b73002c2b423ad33c54
bartosz98765 <wiem@poczta.fm>
2021-02-14 15:34:39 +0100

    Uzupełnienie o plik models

--- reservation_app/models.py
+++ reservation_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Rooms(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    capacity = models.PositiveIntegerField()
+    is_projector = models.BooleanField(default=False)
+
+
+class Reservations(models.Model):
+    date = models.DateField()
+    room = models.ForeignKey(Rooms, on_delete=models.CASCADE, related_name='room_reservation')
+    comment = models.CharField(max_length=128)
+    class Meta:
+        unique_together = ('date', 'room')
